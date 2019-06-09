cd576f879eba1282fe1dc4a148ab88f1c44541ae
whiskpo0 <whiskypo082@gmail.com>
2019-06-08 22:57:55 -0500

    made question model and migration

---
+++ app/Question.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\User; 
+
+class Question extends Model
+{
+    protected $fillable = ['title', 'body']; 
+
+    public function user()
+    { 
+        return $this->belongsTo(User::class); 
+    }
+
+    public function setTitleAttribute($value)
+    { 
+        $this->attributes['title'] = $value;
+        $this->attribures['slug'] = str_slug($value); 
+    }
+}
