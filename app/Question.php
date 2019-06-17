0eadc34dcf549b3bcbf5ce8ea7db28c3b90a1dec
whiskpo0 <whiskypo082@gmail.com>
2019-06-17 13:50:26 -0500

    refactored code and created a trait for votable

--- app/Question.php
+++ app/Question.php
@@ -8,6 +8,8 @@ use App\Answer;
 
 class Question extends Model
 {
+    use VotableTrait; 
+    
     protected $fillable = ['title', 'body']; 
 
     public function user()
@@ -77,20 +79,5 @@ class Question extends Model
     public function getFavoritesCountAttribute()
     {
         return $this->favorites->count(); 
-    }
-
-    public function votes()
-    {
-        return $this->morphToMany(User::class, 'votable');
-    }
-
-    public function upVotes()
-    {
-        return $this->votes()->wherePivot('vote', 1); 
-    }
-    
-    public function downVotes()
-    {
-        return $this->votes()->wherePivot('vote', -1); 
     }
 }
